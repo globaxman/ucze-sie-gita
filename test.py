2c7ea9fab1195223389e82d119f97897029c3e8f
globaxman <zkum1432@gmail.com>
2022-07-17 17:52:51 +0200

    zmiany

--- test.py
+++ test.py
@@ -2,10 +2,6 @@ print('10')
 print('5')
 print('dodanie zmiany')
 print('xdxdxd')
-<< << << < HEAD
-print('44444')
-== == == =
-print('455555')
->>>>>> > 77a70367ae4178f8affecb7c15618683669dabc5
+
 
 its_all = 4
